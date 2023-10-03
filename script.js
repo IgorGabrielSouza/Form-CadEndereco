e378fc54c58d63e6bcad00290d76a226671ce908
Igor Gabriel <igor.liandro@alunoescolassociais.org.br>
2023-10-03 11:44:59 -0300

    atualiza

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ const limparFormulario = (endereco) => {
     document.getElementById('cidade').value = '';
     document.getElementById('estado').value = '';
 }
+
+// Verifica se CEP é válido
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+//Responsável pelo preenchimento do Formulário
+const preencherFormulario = (endereco) => {
+        document.getElementById('rua').value = endereco.lorgradouro;
+        document.getElementById('bairro').value = endereco.bairro;
+        document.getElementById('cidade').value = endereco.localidade;
+        document.getElementById('estado').value = endereco.uf;
+
+}
+// Função para consumo de API da via CEP
+const pesquisaCep = async() => {
+    limparFormulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+
+    if(cepValido(cep.value)){
+        const dados = await fetch(url); // esperar
+        const address = await dados.json(); //retorna dados no formato JSON
+
+        if(address.hasOwProperty('erro')){
+            alert('CEP não encontrado');
+        }else{
+            preencherFormulario(address);
+        }
+    }else{
+        alert('CEP incorreto');
+    }
+}
+
+//Adiciona um evento DOM no input do CEP
+document.getElementById('cep').addEventListener('focusout',pesquisarCep);
